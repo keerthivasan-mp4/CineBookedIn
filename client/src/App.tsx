f43387a3854f6df089f14752462b793eae3144c6
Keerthi Vasan.J <keerthiv741@gmail.com>
2026-07-13 22:15:51 +0530

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import  NavBar from "./component/NavBar"
+import  Footer from "./component/Footer"
+import { Route, Routes } from "react-router-dom"
+import  Home  from "./pages/Home"
+
+
+function App() {
+
+  return (
+    <>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<Home/>}  />
+    </Routes>
+    <Footer/>
+      
+    </>
+  )
+}
+
+export default App
